9086bb28dafe8d9cff9cc4715bd3086f41e9095a
Bruno de Paula <bruno.bert.jj@gmail.com>
2019-09-24 19:51:49 -0300

    changed list

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QTableWidgetItem
 import ui_list
 import sys
 
 
 
-def createTable(ui):
-    ui.tableWidget.setColumnCount(5); 
-  
+def createTable(self):
+    self.tableWidget.setColumnCount(6); 
+    self.tableWidget.setRowCount(2); 
+ 
+    self.tableWidget.setItem(0, 0, QTableWidgetItem("ID"))
+    self.tableWidget.setItem(0, 1, QTableWidgetItem("Chave de Acesso"))
+    self.tableWidget.setItem(0, 2, QTableWidgetItem("CNPJ"))
+    self.tableWidget.setItem(0, 3, QTableWidgetItem("Valor Total"))
+    self.tableWidget.setItem(0, 4, QTableWidgetItem("Data de Emissão"))
+    self.tableWidget.setItem(0, 5, QTableWidgetItem("Status"))
+ 
+    self.tableWidget.setItem(1, 0, QTableWidgetItem("1"))
+    self.tableWidget.setItem(1, 1, QTableWidgetItem("2222 3333 4444 5555 6666 7777 8888 9999 0000"))
+    self.tableWidget.setItem(1, 2, QTableWidgetItem(""))
+    self.tableWidget.setItem(1, 3, QTableWidgetItem(""))
+    self.tableWidget.setItem(1, 4, QTableWidgetItem(""))
+    self.tableWidget.setItem(1, 5, QTableWidgetItem("Pendente"))
+    
+   
 def InitWindow():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = ui_list.Ui_MainWindow()
     ui.setupUi(MainWindow)
-    MainWindow.show()
+    MainWindow.showMaximized()
     createTable(ui)    
     sys.exit(app.exec_())    
     
