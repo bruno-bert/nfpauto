7e0160922d20b8e3c9ad9f448a5dc452f6870b9f
Bruno de Paula <bruno.bert.jj@gmail.com>
2019-09-24 19:23:54 -0300

    22661

--- main.py
+++ main.py
@@ -7,17 +7,19 @@ import sys
 def createTable(ui):
     ui.tableWidget.setColumnCount(5); 
   
-
-if __name__ == "__main__":
+def InitWindow():
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
     ui = ui_list.Ui_MainWindow()
     ui.setupUi(MainWindow)
     MainWindow.show()
-    
-    createTable(ui)
-
+    createTable(ui)    
     sys.exit(app.exec_())    
+    
+
+if __name__ == "__main__":    
+    InitWindow()
+    
     
    
  
